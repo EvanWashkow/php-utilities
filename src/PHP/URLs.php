f66ea99623a18e05ca0e45d2d6d620d50d1031ca
EvanWashkow <epwashkow@gmail.com>
2018-02-14 20:01:54 -0800

    Sanitize URL

--- src/PHP/URLs.php
+++ src/PHP/URLs.php
@@ -18,4 +18,19 @@ class URLs
         $url = filter_var( $url, FILTER_VALIDATE_URL );
         return ( false !== $url );
     }
+    
+    /**
+     * Sanitize URL, returning an empty string if not a valid URL
+     *
+     * @param string $url The URL
+     * @return string Empty string on invalid URL
+     */
+    final public static function Sanitize( string $url )
+    {
+        $url = filter_var( $url, FILTER_SANITIZE_URL );
+        if ( !self::IsValid( $url )) {
+            $url = '';
+        }
+        return $url;
+    }
 }
