dce22805fb13ecb8530eabcb0d813e4e057097bc
EvanWashkow <epwashkow@gmail.com>
2018-02-16 15:57:40 -0800

    Output StackTrace to a string

--- PHP/StackTrace.php
+++ PHP/StackTrace.php
@@ -16,4 +16,46 @@ class StackTrace
     {
         return debug_backtrace();
     }
+    
+    
+    /**
+     * Convert this stack trace into a human-readable string
+     *
+     * @return string
+     */
+    public static function ToString()
+    {
+        // Variables
+        $output     = "\n";
+        $stackTrace = self::Get();
+        
+        // Pop StackTrace entries off
+        array_shift( $stackTrace );
+        array_shift( $stackTrace );
+        
+        // For each stack trace entry, convert it to a string
+        foreach ( $stackTrace as $entry ) {
+            
+            // Variables
+            $caller = $entry[ 'function' ];
+            $file   = '';
+            $line   = '';
+            if ( array_key_exists( 'class', $entry )) {
+                $caller = $entry[ 'class' ] . $entry[ 'type' ] . $caller;
+            }
+            if ( array_key_exists( 'file', $entry )) {
+                $file = $entry[ 'file' ];
+            }
+            if ( array_key_exists( 'line', $entry )) {
+                $line = $entry[ 'line' ];
+            }
+            
+            // Output
+            $output .= "Called by: {$caller}\n";
+            $output .= "File:      {$file}\n";
+            $output .= "Line:      {$line}\n";
+            $output .= "----------------------------------------\n";
+        }
+        return $output;
+    }
 }
